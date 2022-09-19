636270de3a185d425e5b66fbbaebd9f4151ea842
Sam <sambisselele@gmail.com>
2022-09-19 19:34:53 +0100

    Feat(Adding page cover on services page )

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,13 +1,26 @@
+import Head from "next/head";
+import PageCover from "../../components/PageCover/PageCover";
 import DesktopHeader from "../../components/DesktopHeader/DesktopHeader";
 import styles from '../../styles/Home.module.css';
 import Footer from "../../components/Footer/Footer";
 
-const ServicesPage = () => {
+const ServicesPage = ({ props }) => {
+    props = {
+        title: "Agence Vigile Africa ",
+        text: "Une agence de services pour la gestion de vos projets, la sécurité de vos vies et de vos biens",
+        buttonText: "Contactez-nous",
+        image: "/images/business-merger.svg"
+    }
     return (
         <div className={styles.container}>
+            <Head>
+                <title>Agence Vigile Africa | A Propos</title>
+            </Head>
             <DesktopHeader />
             <main className={styles.main}>
-                Services Page
+                <PageCover
+                    props={props}
+                />
             </main>
 
             <Footer />
